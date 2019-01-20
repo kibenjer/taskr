274ccb5cc19792a1981595104ffcddc17d3c708d
Junichirou Kibe <junkibe@gmail.com>
2019-01-20 10:41:59 +0900

    Added tasks table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_105337) do
+ActiveRecord::Schema.define(version: 2019_01_20_011756) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
