c73f379dc5162be4fd988c16289677a6df465dea
Junichiro Kibe <junichirokibe@JunichironoMacBook-Pro.local>
2021-08-27 16:38:58 +0900

    added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+
+  get 'about' => 'pages#about'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
